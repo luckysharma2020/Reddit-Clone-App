7511fcf3babb48a5c65b9d89c18590daf482b735
lucky sharma <76197735+luckysharma2020@users.noreply.github.com>
2021-06-14 01:17:08 +0530

    Add files via upload

---
+++ springredditclone/src/main/java/com/programming/techie/springredditclone/SpringredditcloneApplication.java
@@ -0,0 +1,15 @@
+package com.programming.techie.springredditclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class SpringredditcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringredditcloneApplication.class, args);
+	}
+
+}
